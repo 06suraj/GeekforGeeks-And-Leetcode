ce57b6326c039cf061701a21cc4552be2ca05ca4
06suraj <86453357+06suraj@users.noreply.github.com>
2021-10-04 20:14:48 +0530

    Create 2027. Minimum Moves to Convert String.cpp

---
+++ Weekly Contest 261/2027. Minimum Moves to Convert String.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int minimumMoves(string s) {
+        int n=s.size(),ans=0;
+        for(int i=0;i<n;i++){
+            if(s[i]=='X')
+            {
+                s[i]='O';
+                if(i+1<n)
+                s[i+1]='O';
+                if(i+2<n)
+                s[i+2]='O';
+                ans++;
+            }
+        }
+        return ans;
+    }
+};
